7426e7f7152988328770f05f376504b0f210f983
camilaspt <camilaspitale26@gmail.com>
2024-07-26 19:33:57 -0300

    Cards mascotas

--- frontend-petguardian/src/app/components/card-mascota/card-mascota.component.ts
+++ frontend-petguardian/src/app/components/card-mascota/card-mascota.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzImageModule } from 'ng-zorro-antd/image';
 
 @Component({
   selector: 'app-card-mascota',
   standalone: true,
-  imports: [NzCardModule],
+  imports: [NzCardModule, NzButtonModule, NzIconModule, NzImageModule],
   templateUrl: './card-mascota.component.html',
   styleUrl: './card-mascota.component.scss'
 })
