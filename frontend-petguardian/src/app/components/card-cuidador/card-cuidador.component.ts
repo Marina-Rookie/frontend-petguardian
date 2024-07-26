f8e2fdd989bf68d706675db5791a29b8590ccadb
camilaspt <camilaspitale26@gmail.com>
2024-07-26 19:36:49 -0300

    Creo comp cuidadores

---
+++ frontend-petguardian/src/app/components/card-cuidador/card-cuidador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-cuidador',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-cuidador.component.html',
+  styleUrl: './card-cuidador.component.scss'
+})
+export class CardCuidadorComponent {
+
+}
---
+++ frontend-petguardian/src/app/pages/cuidadores/cuidadores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuidadores',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuidadores.component.html',
+  styleUrl: './cuidadores.component.scss'
+})
+export class CuidadoresComponent {
+
+}
