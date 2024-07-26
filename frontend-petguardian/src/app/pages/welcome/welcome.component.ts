87715495cfa8a502e1c11308df0d3d6534bf76c7
cspitale <camila.spitale@kretz.com.ar>
2024-07-26 15:12:23 -0300

    Update components

--- frontend-petguardian/src/app/pages/welcome/welcome.component.ts
+++ frontend-petguardian/src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
+  imports: [NzButtonModule],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
