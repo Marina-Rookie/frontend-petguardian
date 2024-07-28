b3692a6b5a917b9eb5d9d6b3d4d76c862442915c
camilaspt <camilaspitale26@gmail.com>
2024-07-28 19:29:04 -0300

    Modificamos tarjetas

--- frontend-petguardian/src/app/pages/mascotas/mascotas.component.ts
+++ frontend-petguardian/src/app/pages/mascotas/mascotas.component.ts
@@ -5,11 +5,13 @@ import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { RouterModule } from '@angular/router';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mascotas',
   standalone: true,
-  imports: [NzGridModule, CardMascotaComponent, NzPageHeaderModule, NzSpaceModule, NzButtonModule, RouterModule],
+  imports: [NzGridModule, CardMascotaComponent, NzPageHeaderModule, NzSpaceModule, NzButtonModule, RouterModule, NzRateModule, CommonModule],
   templateUrl: './mascotas.component.html',
   styleUrl: './mascotas.component.scss'
 })
