c3cce66a5bd536aea84bebe405cacb0973e328f2
emadelbianco <emanuel.delbianco@gmail.com>
2024-09-05 21:16:45 -0300

    Agrego formulario de registro y login

--- frontend-petguardian/src/app/pages/login/login.component.ts
+++ frontend-petguardian/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -27,8 +27,9 @@ import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  loginForm;
   formLogin: boolean = true;
+  registerForm: FormGroup = new FormGroup({});
+  loginForm: FormGroup = new FormGroup({});
   checked = false;
 
   constructor(
@@ -38,23 +39,51 @@ export class LoginComponent {
     private router: Router
   ) {
     this.localStorage.crearValues();
+  }
+
+  ngOnInit(): void {
+    this.initForms();
+  }
+
+  initForms(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
+
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      cuidador: [false, [Validators.required]],
+    });
   }
 
-
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      this.service.post(this.loginForm.value, 'usuarios/login').subscribe({
+  onSubmitRegister(): void {
+    if (this.registerForm.valid) {
+      this.service.post(this.registerForm.value, 'usuarios').subscribe({
         next: (data: any) => {
-          this.localStorage.setItem('token', data['token']);
-          this.localStorage.setItem('idUsuario', data['idUsuario']);
-          this.localStorage.setItem('rol', data['rol']);
-          this.router.navigate(['/mascotas']);
+          this.setItemsAndNavigate(data);
         },
       });
     }
+  }
+
+  onSubmitLogin(): void {
+    if (this.loginForm.valid) {
+      this.service.post(this.loginForm.value, 'usuarios/login').subscribe({
+        next: (data: any) => {
+          this.setItemsAndNavigate(data);
+        },
+      });
+    }
+  }
+
+  setItemsAndNavigate(data: any): void {
+    this.localStorage.setItem('token', data['token']);
+    this.localStorage.setItem('idUsuario', data['idUsuario']);
+    this.localStorage.setItem('rol', data['rol']);
+    this.router.navigate(['/mascotas']);
   }
 }
