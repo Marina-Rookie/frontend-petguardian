3447ed7687071f51616eca403b09707a32ce9f33
cspitale <camila.spitale@kretz.com.ar>
2024-09-23 22:26:36 -0300

    Eliminar mascota

--- frontend-petguardian/src/app/components/card-mascota/card-mascota.component.ts
+++ frontend-petguardian/src/app/components/card-mascota/card-mascota.component.ts
@@ -5,6 +5,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { Mascota } from '../../models/Mascota';
 import { ModalService } from '../../services/shared/modals.service';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-card-mascota',
@@ -16,12 +17,23 @@ import { ModalService } from '../../services/shared/modals.service';
 export class CardMascotaComponent implements OnInit {
   @Input() mascota: Mascota | undefined;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private service: ApiService) {}
 
   ngOnInit(): void {}
 
   editMascota(){
     this.modalService.showModal();
     this.modalService.setMascotaEditModal(this.mascota);
+  }
+
+  eliminarMascota() {
+    this.service.delete('mascotas/' + this.mascota?._id).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 }
