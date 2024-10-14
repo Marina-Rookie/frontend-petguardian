b09156bdf8967e4dbb52d70437ae7c1a9ddb77f5
camilaspt <camilaspitale26@gmail.com>
2024-10-14 18:52:53 -0300

    Crear reserva

--- src/app/components/modal-reserva/modal-reserva.component.ts
+++ src/app/components/modal-reserva/modal-reserva.component.ts
@@ -2,8 +2,15 @@ import { Component, Input } from '@angular/core';
 import { NgZorroModule } from '../../ngzorro.module';
 import { ModalService } from '../../services/shared/modals.service';
 import { ApiService } from '../../services/api.service';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LocalStorageService } from '../../services/localstorage.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-modal-reserva',
@@ -13,7 +20,6 @@ import { LocalStorageService } from '../../services/localstorage.service';
   styleUrl: './modal-reserva.component.scss',
 })
 export class ModalReservaComponent {
-
   @Input() cuidador: any = {};
 
   formReserva: FormGroup = new FormGroup({});
@@ -26,7 +32,8 @@ export class ModalReservaComponent {
     private modalService: ModalService,
     private apiService: ApiService,
     private fb: FormBuilder,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private msg: NzMessageService
   ) {}
 
   ngOnInit() {
@@ -44,35 +51,55 @@ export class ModalReservaComponent {
       fechaFin: [null, [Validators.required]],
       horaTurno: [null, [Validators.required]],
       mascotas: [null, [Validators.required]],
+      clienteId: [this.idUsuario],
+      cuidador: [this.cuidador._id],
       comentario: [null],
     });
   }
 
   getTurnosDisponibles() {
-    console.log(this.turnosDisponibles)
-    if(this.formReserva.get('fechaInicio') && this.formReserva.get('fechaFin')){
+    console.log(this.turnosDisponibles);
+    if (
+      this.formReserva.get('fechaInicio') &&
+      this.formReserva.get('fechaFin')
+    ) {
       const body = {
         cuidadorId: this.cuidador._id,
         fechaInicio: this.formReserva.get('fechaInicio')?.value,
-        fechaFin: this.formReserva.get('fechaFin')?.value
-      }
+        fechaFin: this.formReserva.get('fechaFin')?.value,
+      };
       console.log(body);
-      this.apiService.post(body, 'turnos/disponibilidad').subscribe((res: any) => {
-        console.log(res);
-        this.turnosDisponibles = res;
-      });
+      this.apiService
+        .post(body, 'turnos/disponibilidad')
+        .subscribe((res: any) => {
+          console.log(res);
+          this.turnosDisponibles = res;
+        });
     }
   }
 
   getMascotasUsuario() {
-    this.apiService.get('mascotas/mascotasPorUsuario/' + this.idUsuario).subscribe((res: any) => {
-      this.mascotas = res;
+    this.apiService
+      .get('mascotas/mascotasPorUsuario/' + this.idUsuario)
+      .subscribe((res: any) => {
+        this.mascotas = res;
+      });
+  }
+
+  postFormReserva() {
+    this.apiService.post(this.formReserva.value, 'reservas').subscribe({
+      next: (res: any) => {
+        this.msg.create('success', 'Reserva realizada con éxito');
+      },
+      error: (error) => {
+        this.msg.create('error', error.error.message);
+      },
     });
   }
-  printform() {
-    console.log(this.formReserva.value);
-  }
+
   handleOk(): void {
+    console.log(this.formReserva.value);
+    this.postFormReserva();
     this.modalService.hideReservaModal();
   }
 
