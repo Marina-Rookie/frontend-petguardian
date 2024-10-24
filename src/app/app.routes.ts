9a6f6c2e8a467760180edd7a7e744fd4056b24d1
emadelbianco <emanuel.delbianco@gmail.com>
2024-10-23 21:01:03 -0300

    Seguridad en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,12 @@ import { ReservaComponent } from './pages/reserva/reserva.component';
 import { NuevaMascotaComponent } from './pages/nueva-mascota/nueva-mascota.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { AuthGuardService } from './services/auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'cuidadores', component: CuidadoresComponent},
-  { path: 'reservas', component: ReservaComponent},
-  { path: 'cuidadores/reserva', component: ReservaComponent},
-  { path: 'mascotas/nueva', component: NuevaMascotaComponent},
-  { path: 'perfil', component: PerfilComponent},
+  { path: 'cuidadores', component: CuidadoresComponent, canActivate: [AuthGuardService]},
+  { path: 'reservas', component: ReservaComponent, canActivate: [AuthGuardService]},
+  { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuardService]},
   { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
