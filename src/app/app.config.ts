8c5fc0603a0b30636bbc77addc93205b0b18449c
camilaspt <camilaspitale26@gmail.com>
2024-10-28 19:01:15 -0300

    idioma app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideNzIcons } from './icons-provider';
-import { ca_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { ca_ES, es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import ca from '@angular/common/locales/ca';
 import { FormsModule } from '@angular/forms';
@@ -22,7 +22,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNzIcons(),
-    provideNzI18n(ca_ES),
+    provideNzI18n(es_ES),
     importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([authInterceptor])),
