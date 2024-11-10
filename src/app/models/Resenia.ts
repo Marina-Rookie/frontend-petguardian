21d2c9d3e1452ecfc24587d9b7be1fa090bdeee0
camilaspt <camilaspitale26@gmail.com>
2024-11-10 19:31:13 -0300

    Fix resenia

--- src/app/models/Resenia.ts
+++ src/app/models/Resenia.ts
@@ -1,11 +1,9 @@
 export class Resenia {
-  _id: string;
   reserva: string;
   puntuacion: number;
   comentario: string;
 
-  constructor(id: string, reserva: string, puntuacion: number, comentario: string) {
-    this._id = id;
+  constructor(reserva: string, puntuacion: number, comentario: string) {
     this.reserva = reserva;
     this.puntuacion = puntuacion;
     this.comentario = comentario;
